d484c1e36cac1d984e22517b070a3245900e6452
Nikki Waelkens <nikkiwaelkens@Nikkis-MBP.modem>
2020-03-13 20:57:45 +1100

    Edited strings outputted to user and pseudocode

--- Code.rb
+++ Code.rb
@@ -20,15 +20,18 @@ end
 puts
 puts
 pastel = Pastel.new
-puts pastel.yellow(Font.write("Where  To  Eat?"))
+#title
+puts pastel.yellow(Font.write("Where  To  Eat?")) #title
 
-continue = true
+continue = true #flag condition
 
+# user details method to call if user is in Sydney
 def user_details
     organise()
     puts "\n\nGreat! #{$emoji_list[2]}  Lets start off with your name?"
     user_name = gets.chomp
         
+    # error handling - while loop outputting error if numbers/symbols used
         while !user_name.match(/^[[:alpha:]]+$/)
             organise()
             puts "\n\nSorry#{$emoji_list[1]}"
@@ -39,14 +42,17 @@ def user_details
     puts "\n\nHey #{user_name}!"
 end
 
+# welcome message
 puts "\nWelcome! Let's find you somewhere good to eat.\n#{$emoji_list[7..-1].join" "}".colorize(:light_blue)
 
 choice = {'Yes' => true, 'No' => false}
 user_in_sydney = Prompt.select("\nFirstly, are you located in Sydney?", choice)
 
+# if statement - if conditon is false to end program
 if user_in_sydney != true
     organise()
-    continue = false
+    # flag value changed
+    continue = false 
     puts "\n\nSorry  #{$emoji_list[0]} , we're only giving recommendations for Sydney for now."
     puts "Check back soon!"
     organise()
@@ -54,6 +60,7 @@ else
 user_details
 end
 
+# while loop begins
 while continue == true
 
     sydney_regions = {"CBD" => CBD, "Inner West" => Inner_west, "Inner City" => Inner_city, "Northern Sydney" => Northern_sydney, "Eastern Suburbs" => Eastern_suburbs, "Southern Sydney" => Southern_sydney, "Western Sydney" => Western_sydney}
@@ -69,15 +76,17 @@ while continue == true
             
     organise()
         
+    # for loop - output chosen region restaurants array at index relevant to cuisine
         for i in 0..chosen_region.restaurants.length-1
             result = chosen_region.restaurants[i]
+            # if statement - output following message if user doesn't choose "suprise me"
             if chosen_region.restaurants[i].cuisine == chosen_cuisine && chosen_region.restaurants[i].cuisine != "Suprise me"
             result = chosen_region.restaurants[i]
             puts "\n\n#{chosen_cuisine}! Delicious #{$emoji_list[4]}  Here's your restaurant..\n"
             puts result.name.colorize(:light_blue)
             puts "\nAnd here's the address..\n"
             puts result.address.colorize(:light_blue)
-            
+            # if user chooses "suprise me" print last restauraunt in restaurants array in chosen region
             elsif chosen_cuisine == "Suprise me" 
                 result = chosen_region.restaurants[-1]
                 puts "\n\nOK! If you say so..\n\n"
@@ -86,23 +95,23 @@ while continue == true
                 puts "\nAnd here's the address..\n"
                 puts result.address.colorize(:light_blue)
                 puts
+                # break the loop so only last restauraunt in the array is outputted
                 break
             end
         end
-
+# if the user wants to try again while loop continues
     re_try = {'Yes' => true, 'No' => false}
     try_again = Prompt.select("\nFeel like something else #{$emoji_list[5]} ?", re_try)
-
+# if user doesn't want to try again end message and change flag value to end loop
     if try_again == false
         organise()
-        puts "\n\nEnjoy your meal at #{result.name} hun  #{$emoji_list[6]} "
+        puts "\n\nEnjoy your meal hun  #{$emoji_list[6]} "
         puts "Come back soon for another delicious restauraunt recommendation!\n"
         puts $emoji_list[7..-1].join" "
         organise()
-    continue = false
+    continue = false #flag condition set to false to end while loop
     else
         organise()
     puts
     end
 end
-
